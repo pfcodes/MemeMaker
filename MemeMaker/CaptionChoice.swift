add209a5157a1f08b1727ff49882b1bd06ad16f3
Phil Fevry <phil@auroradigit.al>
2020-02-21 21:45:08 -0800

    implementation

---
+++ MemeMaker/CaptionChoice.swift
@@ -0,0 +1,14 @@
+//
+//  CaptionChoice.swift
+//  MemeMaker
+//
+//  Created by Phil on 2/21/20.
+//  Copyright © 2020 AURORA Digital. All rights reserved.
+//
+
+import Foundation
+
+struct CaptionChoice {
+  let emoji: String
+  let caption: String
+}
--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -14,15 +14,59 @@ class ViewController: UIViewController {
   @IBOutlet weak var bottomCaptionLabel: UILabel!
   @IBOutlet weak var bottomSegmentedControl: UISegmentedControl!
   
+  let topChoices = [
+    CaptionChoice(emoji: "🕶", caption: "You know what's cool?"),
+    CaptionChoice(emoji: "💥", caption: "You know what makes me mad?"),
+    CaptionChoice(emoji: "💕", caption: "You know what I love?")
+  ]
+
+  let bottomChoices = [
+    CaptionChoice(emoji: "🐱", caption: "Cats wearing hats"),
+    CaptionChoice(emoji: "🐕", caption: "Dogs carrying logs"),
+    CaptionChoice(emoji: "🐒", caption: "Monkeys being funky")
+  ]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
-
-  @IBAction func topSegmentedControlValueChanged(_ sender: Any) {
+    prepareSegmentedControls()
+    updateCaptionLabels()
   }
   
-  @IBAction func bottomSegmentedControlValueChanged(_ sender: Any) {
+  func prepareSegmentedControls() {
+    topSegmentedControl.removeAllSegments()
+    for choice in topChoices {
+      topSegmentedControl.insertSegment(withTitle: choice.emoji, at: topChoices.count, animated: false)
+    }
+    topSegmentedControl.selectedSegmentIndex = 0
+    
+    bottomSegmentedControl.removeAllSegments()
+    for choice in bottomChoices {
+      bottomSegmentedControl.insertSegment(withTitle: choice.emoji, at: bottomChoices.count, animated: false)
+    }
+    bottomSegmentedControl.selectedSegmentIndex = 0
+  }
+  
+  func updateTopCaptionLabel() {
+    topCaptionLabel.text = topChoices[topSegmentedControl.selectedSegmentIndex].caption
+  }
+  
+  func updateBottomCaptionLabel() {
+    bottomCaptionLabel.text = bottomChoices[bottomSegmentedControl.selectedSegmentIndex].caption
+  }
+  
+  func updateCaptionLabels() {
+    updateTopCaptionLabel()
+    updateBottomCaptionLabel()
+  }
+  
+  
+
+  @IBAction func topSegmentedControlValueChanged(_ sender: UISegmentedControl) {
+    updateTopCaptionLabel()
+  }
+  
+  @IBAction func bottomSegmentedControlValueChanged(_ sender: UISegmentedControl) {
+    updateBottomCaptionLabel()
   }
 }
 
