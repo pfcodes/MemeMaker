abd43e1ef66bb5954dead2e75f3a5fee1702d4ea
Phil Fevry <phil@auroradigit.al>
2020-02-21 21:20:00 -0800

    create actions and outlets

--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var topSegmentedControl: UISegmentedControl!
+  @IBOutlet weak var topCaptionLabel: UILabel!
+  @IBOutlet weak var bottomCaptionLabel: UILabel!
+  @IBOutlet weak var bottomSegmentedControl: UISegmentedControl!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
-
+  @IBAction func topSegmentedControlValueChanged(_ sender: Any) {
+  }
+  
+  @IBAction func bottomSegmentedControlValueChanged(_ sender: Any) {
+  }
 }
 
